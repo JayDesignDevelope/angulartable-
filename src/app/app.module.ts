d70837ebfb4a834355b158b57410cee721b13006
Jay Vinay <jay.design.developer@gmail.com>
2023-08-06 00:12:54 +0530

    service file dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableDemoComponent } from './table-demo/table-demo.component';
+import {MatTableModule} from "@angular/material/table";
+import {HttpClientModule} from "@angular/common/http";
+import {DataService} from "./data.service";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableDemoComponent
   ],
-  imports: [
-    BrowserModule
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatTableModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatIconModule
+    ],
+  providers: [
+    DataService
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
